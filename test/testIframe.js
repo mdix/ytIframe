fae55d22268a861e2b07be4243d26f0efba6c58c
mdix <dix@mediaman.de>
2012-02-26 14:07:21 +0100

    Added test for passing full youtube URL instead of hash

--- test/testIframe.js
+++ test/testIframe.js
@@ -1,6 +1,56 @@
-describe('YtIframe instantiated with a string', function() {
+describe('YtIframe instantiated with a videoHash string', function() {
     var videoHash = 'w4yGJEU85wY';
     var iframe = YtIframe(videoHash);
+
+    it('returns an IFRAME node object', function() {
+        expect(iframe.nodeName).toEqual('IFRAME');
+    });
+
+    it('which has a width property', function() {
+        expect(iframe.hasAttribute('width')).toBeTruthy();
+    });
+
+    it('where with is the default value of 560', function() {
+        expect(iframe.width).toEqual('560');
+    });
+
+    it('which has a height property', function() {
+        expect(iframe.hasAttribute('height')).toBeTruthy();
+    });
+
+    it('where height is the default value of 315', function() {
+        expect(iframe.height).toEqual('315');
+    });
+
+    it('which has a src property', function() {
+        expect(iframe.hasAttribute('src')).toBeTruthy();
+    });
+
+    it('that consists of the youtube embed link and the passed video hash', function() {
+        expect(iframe.src).toMatch('http://www.youtube.com/embed/' + videoHash);
+    })
+
+    it('which has a frameBorder property', function() {
+        expect(iframe.hasAttribute('frameBorder')).toBeTruthy();
+    });
+
+    it('that is set to 0', function() {
+        expect(iframe.frameBorder).toEqual('0');
+    });
+
+    it('which has a allowFullScreen property', function() {
+        expect(iframe.hasAttribute('allowFullScreen')).toBeTruthy();
+    });
+
+    it('that is a empty string', function() {
+        expect(iframe.getAttribute('allowFullScreen')).toEqual("");
+    });
+});
+
+describe('YtIframe instantiated with a full url string', function() {
+    var videoURL  = 'http://www.youtube.com/watch?v=pqaARDsiJv4';
+    var videoHash = 'pqaARDsiJv4';
+    var iframe    = YtIframe(videoURL);
 
     it('returns an IFRAME node object', function() {
         expect(iframe.nodeName).toEqual('IFRAME');
